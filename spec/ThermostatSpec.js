53b179d8146589873ac77879508f9b891f3be11c
Josh Hemsley <josh@breathehr.com>
2019-07-24 17:22:11 +0100

    completed Thermostat JS Files - Kaja is always right, don't try and argue otherwise....

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -39,13 +39,55 @@ describe("Thermostat", function() {
         thermostat.switchPowerSavingModeOn();
         expect(thermostat.isPowerSavingModeOn()).toBe(true);
       });
+      it('can be reset to the default temperature', function() {
+        for (var i = 0; i < 6; i++) {
+          thermostat.increase();
+        };
+        thermostat.resetTemperature();
+        expect(thermostat.getCurrentTemperature()).toEqual(20);
+      });
+    describe('when power saving mode is on', function() {
+      it('has a maximum temeprature of 25 degrees', function () {
+        for (var i =0; i < 6; i++) {
+          thermostat.increase();
+        }
+        expect(thermostat.getCurrentTemperature()).toEqual(25);
+      });
+    });
+    describe('wehn power saving mode is off', function() {
+      it('has a maximum temeprature of 32 degrees', function () {
+        thermostat.switchPowerSavingModeOff();
+        for (var i = 0; i < 13; i++) {
+          thermostat.increase();
+        }
+        expect(thermostat.getCurrentTemperature()).toEqual(32);
+      });
+    });
+    describe('displaying usage levels', function() {
+      describe('when the temperature is below 18 degrees', function() {
+        it('it is considered low-usage', function() {
+          for (var i = 0; i < 3; i++) {
+            thermostat.decrease();
+          }
+          expect(thermostat.energyUsage()).toEqual('low-usage');
+        });
+      });
+
+      describe('when the temperature is between 18 and 25 degrees', function() {
+        it('it is considered medium-usage', function() {
+          expect(thermostat.energyUsage()).toEqual('medium-usage');
+        });
+      });
+
+      describe('when the temperature is anything else', function() {
+        it('it is considered high-usage', function() {
+          thermostat.powerSavingMode = false;
+          for (var i = 0; i < 6; i++) {
+            thermostat.increase();
+          };
+          expect(thermostat.energyUsage()).toEqual('high-usage');
+        });
+      });
+    });
   });
-
-
-
-
-
-
-
-
 });
